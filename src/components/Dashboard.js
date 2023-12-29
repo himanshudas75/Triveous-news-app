5b0bb59d82502cbd4be1606f12102c20c2e42192
Harsha Agrawal <harsha239>
2023-12-29 16:41:12 +0530

    Title-change

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -54,7 +54,7 @@ const createSlug = (title) => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg" >
-      <a class="navbar-brand text" href="#">Triveous</a>
+      <a class="navbar-brand text" href="#">News App</a>
 
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
