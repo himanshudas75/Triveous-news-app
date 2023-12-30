f3a2a8631ab468f7049a4b4aa65e7d7e9739cfee
Himanshu Das <himanshudas75@gmail.com>
2023-12-30 13:36:29 +0530

    built app

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Home from "./components/Home";
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Register from './components/Register';
+import NewsDetail from './components/NewsDetail';
+import { AuthProvider } from './components/Auth';
+
+const App = () => {
+    return (
+        <AuthProvider>
+            <Router>
+                <Routes>
+                    <Route exact path="/" element={<Register />} />
+                    <Route exact path="/dashboard" element={<Dashboard />} />
+                    <Route exact path="/login" element={<Login />} />
+                    <Route path="/dashboard/:title" element={<NewsDetail />} />
+                    {/* <Route exact path="/signup" component={SignUp} /> */}
+                </Routes>
+            </Router>
+        </AuthProvider>
+    );
+};
+export default App;
